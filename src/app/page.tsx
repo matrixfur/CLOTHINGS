78d71f7b804234a39bd3373d875a0845ac144fe8
ansarhussain <a.ansarhussain2004@gmail.com>
2026-01-08 11:53:01 +0000

    not visible

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
-import Image from 'next/image';
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-white p-4">
-      <Image
-        src="/kraft-box.jpg"
-        alt="4ARTH Kraft Box"
-        width={800}
-        height={600}
-      />
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img src="/kraft-box.jpg" alt="4ARTH Kraft Box" width="800" height="600" />
     </main>
   );
 }
