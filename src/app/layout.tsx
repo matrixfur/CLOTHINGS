dda05c2e9d43900723339ad5a94d71e20ab6c642
ansarhussain <a.ansarhussain2004@gmail.com>
2026-01-08 11:54:58 +0000

    not visible

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type {Metadata} from 'next';
 import './globals.css';
-import { Toaster } from '@/components/ui/toaster';
 
 export const metadata: Metadata = {
   title: '4arth Landing Page',
@@ -14,14 +13,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Literata:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet" />
-      </head>
-      <body className="font-body antialiased bg-background">
+      <body>
         {children}
-        <Toaster />
       </body>
     </html>
   );
