e8ffb2d627e40d74127503647c1e33fac1d26df8
ansarhussain <a.ansarhussain2004@gmail.com>
2026-01-08 11:51:57 +0000

    still image is not visible

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-white p-4">
       <div className="w-full max-w-sm">
         <Image
-          src="/4ARTH-KRAFT BOX_page-0001.jpg"
+          src="/kraft-box.jpg"
           alt="4ARTH Kraft Box"
           width={800}
           height={600}
-          className="object-contain w-full h-full"
+          className="h-full w-full object-contain"
         />
       </div>
     </main>
