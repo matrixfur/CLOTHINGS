edb2f2284e2fd95082c3dc07183ef9870826fd48
ansarhussain <a.ansarhussain2004@gmail.com>
2026-01-08 11:52:29 +0000

    not visible

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,12 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-white p-4">
-      <div className="w-full max-w-sm">
-        <Image
-          src="/kraft-box.jpg"
-          alt="4ARTH Kraft Box"
-          width={800}
-          height={600}
-          className="h-full w-full object-contain"
-        />
-      </div>
+      <Image
+        src="/kraft-box.jpg"
+        alt="4ARTH Kraft Box"
+        width={800}
+        height={600}
+      />
     </main>
   );
 }
