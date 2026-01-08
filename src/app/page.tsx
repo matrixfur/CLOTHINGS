5a543387625558172d30d486fcf8f39a5fe36829
ansarhussain <a.ansarhussain2004@gmail.com>
2026-01-08 12:00:39 +0000

    only it showing this on the output

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-background p-24">
+    <main>
       <Image
-        src="/box-image.jpg"
+        src="/BOX_page-0001.jpg"
         alt="Kraft Box"
         width={800}
         height={800}
